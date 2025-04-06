d2f4c63bf811b1ad350cabf6699c8f7ef5fb1ca5
MohsinRehman12 <mshakeelrehman@hotmail.com>
2025-04-05 20:53:54 -0400

    small cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import socialmo1 from './assets/socialmo1.jpg'
-import socialmo2 from './assets/socialmo2.jpg'
-import socialmo3 from './assets/socialmo3.jpg'
 import NavbarDiv from './components/Navbar';
-import Skills from './components/Skills';
 import { useEffect, useRef, useState } from 'react';
-import { Button } from '@mui/material';
-import About from './components/About';
-import Banner from './components/Banner';
-import { MarkunreadOutlined } from '@mui/icons-material';
-import {AiFillYoutube} from 'react-icons/ai'
-import Socialmo from './components/projects/Socialmo';
-import Bookstore from './components/projects/Bookstore';
-import Contentbased from './components/projects/Contentbased';
-import Seperator from './components/Seperator';
 import { Box, Grid } from '@mui/material';
 import Socialmoproj from './components/projects/Socialmoproj';
 import Contentbasedproj from './components/projects/Contentbasedproj';
